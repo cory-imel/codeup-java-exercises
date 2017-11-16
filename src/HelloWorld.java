a74244f2c08c373f1a54c1d653e3c975920d749d
Cory Imel <cory.imel@gmail.com>
2017-11-16 15:49:12 -0600

    added STV exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,17 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 2;
+        System.out.println(myFavoriteNumber);
+        String mySting = "d";
+        System.out.println(mySting);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
     }
 }
 
