88e23b0f04df5179061717e96443381bd1f76e2d
Cory Imel <cory.imel@gmail.com>
2017-11-20 14:00:40 -0600

    updated exercise check

--- src/exercise_check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-files=(HellowWorld.java ConsoleExercises.java ControlFlowExercises.java StringExercise.java)
-  
-for file in "${files[@]}"
-do
-    if ! [ -e "$file" ]
-    then echo "$file is missing";
-    fi
-done
