940282bdfe2840781c8f8b7c6fb179d22562fc03
Cory Imel <cory.imel@gmail.com>
2017-11-21 10:45:29 -0600

    Finished getInteger

--- out/production/codeup-java-exercises/exercise_check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-files=(HelloWorld.java ConsoleExercises.java ControlFlowExercises.java StringExercise.java Bob.java MethodsExercises.java)
-
-for file in "${files[@]}"
-do
-    if ! [ -e "$file" ]
-    then echo "$file is missing";
-    fi
-done
--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -4,26 +4,25 @@ class MethodsExercises {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please input the first number of the operation");
-        double firstNum = sc.nextDouble();
+        int firstNum = sc.nextInt();
         System.out.println("Please input the second number of the operation");
-        double secondNum = sc.nextDouble();
+        int secondNum = sc.nextInt();
 
         System.out.println(add(firstNum,secondNum));
         System.out.println(subtract(firstNum,secondNum));
         System.out.println(multiply(firstNum,secondNum));
         System.out.println(divide(firstNum,secondNum));
         System.out.println(modulus(firstNum,secondNum));
-
+        System.out.println(getInteger(1,10,sc));
 
     }
-
-    private static double add(double num1, double num2){
+    private static int add(int num1, int num2){
         return num1 + num2;
     }
-    private static double subtract(double num1, double num2){
+    private static int subtract(int num1, int num2){
         return num1 - num2;
     }
-    private static double multiply(double num1, double num2){
+    private static int multiply(int num1, int num2){
         if (num1 != 0 && num2 != 0) {
             if (num2 > 0) {
                 return multiply(num1, num2 - 1) + num1;
@@ -33,7 +32,7 @@ class MethodsExercises {
         }
         return 0;
     }
-    private static double divide(double num1, double num2){
+    private static int divide(int num1, int num2){
         if (num1 != 0 && num2 != 0) {
                 return num1 / num2;
         }else{
@@ -41,12 +40,20 @@ class MethodsExercises {
             return 0;
         }
     }
-    private static double modulus(double num1, double num2) {
+    private static int modulus(int num1, int num2) {
         if (num1 != 0 && num2 != 0) {
             return num1 % num2;
         } else {
             System.out.println("Does not compute!");
             return 0;
         }
+    }
+    private static int getInteger(int min, int max, Scanner sc){
+        System.out.printf("Enter a number between %d and %d: ",min, max);
+        int input = sc.nextInt();
+        if(!(input >= min && input <= max)){
+           input = getInteger(min, max, sc);
+        }
+        return input;
     }
 }
