fd512a890a8f7b69859c21f57fc214a9ffd5eacd
Cory Imel <cory.imel@gmail.com>
2017-11-27 18:43:49 -0600

    removed output folder from repo

--- out/production/codeup-java-exercises/exercise_check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-files=(HelloWorld.java ConsoleExercises.java ControlFlowExercises.java StringExercise.java Bob.java MethodsExercises.java HighLow.java Person.java util/Input.java)
-
-for file in "${files[@]}"
-do
-    if ! [ -e "$file" ]
-    then echo "$file is missing";
-    fi
-done
