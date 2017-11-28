4d0fdba299baff01ce897974b0abe29ac4c24a82
Cory Imel <cory.imel@gmail.com>
2017-11-27 18:31:15 -0600

    added rest of oop exercises

---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,18 @@
+package shapes;
+
+import util.Input;
+
+public class CircleApp {
+    public static void main(String[] args) {
+        System.out.println("Let's create a circle!");
+
+        System.out.println("Please provide a radius for your circle");
+        double radius = Input.getInt();
+
+        Circle myCircle = new Circle(radius);
+
+        System.out.println("Here's the area: " + myCircle.getArea());
+
+        System.out.println("Here's the circumference: " + myCircle.getCircumference());
+    }
+}
